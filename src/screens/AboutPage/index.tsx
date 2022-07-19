85a7297294c7c20a95b20562e570aec8fd7a8b2f
rhaneyko <rhaneykohonorio@gmail.com>
2022-07-19 11:57:24 -0400

    React App

--- src/screens/AboutPage/index.tsx
+++ src/screens/AboutPage/index.tsx
@@ -1,23 +1,20 @@
-import { 
-    Container,
-    Title,
-    About,
-    TextAbout,
-     } from './styles'
-
+import { Container, Title, About, TextAbout } from "./styles";
 
 const AboutPage: React.FC = () => {
   return (
-    <Container id='about' >
+    <Container id="about">
       <About>
         <Title>Quem eu sou?</Title>
-         <TextAbout>
-            Meu nome é Rhaneyko Honorio, tenho 19 anos, sou desenvolvedor Mobile | Front-End. Atualmente sou estagiário na Unimed. Venho aprimorando meus conhecimentos em JavaScript, React JS, React Native e TypeScript. Estou cursando Análise e Desenvolvimento de Sistemas na Faculdade Estácio, atualmente no primeiro semestre.
-         </TextAbout>
+        <TextAbout>
+          Meu nome é Rhaneyko Honorio, tenho 19 anos, sou desenvolvedor Mobile |
+          Front-End. Atualmente sou estagiário na Unimed. Venho aprimorando meus
+          conhecimentos em JavaScript, React JS, React Native e TypeScript.
+          Estou cursando Análise e Desenvolvimento de Sistemas na Faculdade
+          Estácio, atualmente no primeiro semestre.
+        </TextAbout>
       </About>
     </Container>
-  )
-}
+  );
+};
 
 export default AboutPage;
-
